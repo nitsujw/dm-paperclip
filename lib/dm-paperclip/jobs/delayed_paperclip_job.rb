816a1c8d48fff06af7b9db89847927e370ee96c0
Paul Schuegraf <paul.schuegraf@gmail.com>
2010-11-21 23:41:23 -0800

    removed debug from delayed job

--- lib/dm-paperclip/jobs/delayed_paperclip_job.rb
+++ lib/dm-paperclip/jobs/delayed_paperclip_job.rb
@@ -1,27 +1,19 @@
 class DelayedPaperclipJob < Struct.new(:instance_klass, :instance_id, :attachment_name)
-  def perform
-    process_job do
-			begin
-	      instance.send(attachment_name).reprocess!
-	      instance.send("#{attachment_name}_processed!")
-			rescue Exception => e
-				log = Logger.new(STDOUT)
-				log.debug e.message
-				e.backtrace.each do |i|
-					log.debug i
-				end				
-			end
-    end
-  end
-  
-  private
-  def instance
-    @instance ||= instance_klass.constantize.get(instance_id)
-  end
-  
-  def process_job
-    instance.send(attachment_name).job_is_processing = true
-    yield
-    instance.send(attachment_name).job_is_processing = false    
-  end
+	def perform
+		process_job do
+			instance.send(attachment_name).reprocess!
+			instance.send("#{attachment_name}_processed!")
+		end
+	end
+
+	private
+	def instance
+		@instance ||= instance_klass.constantize.get(instance_id)
+	end
+
+	def process_job
+		instance.send(attachment_name).job_is_processing = true
+		yield
+		instance.send(attachment_name).job_is_processing = false    
+	end
 end
